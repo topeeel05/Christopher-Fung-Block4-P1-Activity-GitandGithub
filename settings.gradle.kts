e6c2fd4a949aafa02cb6d9802b714ad155670be9
topeeel05 <topelfungml@gmail.com>
2023-12-09 20:42:09 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ChristopherFung-Block4-Quiz"
+include(":app")
+ 
